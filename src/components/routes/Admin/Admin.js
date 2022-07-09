a64ea305e11ce470e6aea10f569126af0b0952a3
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-09 20:21:13 +0600

    admin page setup with panel route and slidebar route

--- src/components/routes/Admin/Admin.js
+++ src/components/routes/Admin/Admin.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Panel from '../Shared/Panel';
+import Sidebar from '../Shared/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const Admin = () => {
+    const [midTitle, setMidTitle] = useState('Service List');
+
+    const menuItems = [
+        {
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-table" viewBox="0 0 16 16">
+                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z" />
+            </svg>,
+            path: '/admin',
+            name: 'Service List'
+        },
+        {
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+            </svg>,
+            path: '/admin/addService',
+            name: 'Add Service'
+        },
+        {
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-plus" viewBox="0 0 16 16">
+                <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+            </svg>,
+            path: '/admin/makeAdmin',
+            name: 'Make Admin'
+        }
+    ];
+
     return (
-        <div>
-            This is Admin.
-        </div>
+        <section>
+            <Panel title={midTitle} />
+            <Sidebar menuItems={menuItems} setMidTitle={setMidTitle} />
+            <Outlet />
+        </section>
     );
 };
 
