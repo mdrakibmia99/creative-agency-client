154b8d86c86afbb4095b2a45fd7399f1f97966c3
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-09 20:37:18 +0600

    useauthenticate for check user

---
+++ src/components/routes/Authenticate/useAuthenticate.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function useAuthenticate(user) {
+    const [token, setToken] = useState('');
+    useEffect(() => {
+        console.log(user);
+        const email = user?.user?.email;
+
+        if (email) {
+            const name = user?.user?.displayName;
+
+            const { data } = axios.put(`http://localhost:5000//user/${email}`, { name, email });
+            console.log(data);
+
+            if (name) {
+                const accessToken = JSON.stringify(user?.user?.accessToken)
+                localStorage.setItem('accessToken', accessToken);
+                setToken(accessToken);
+            }
+        }
+    }, [user]);
+
+    return [token];
+}
+
+export default useAuthenticate;
